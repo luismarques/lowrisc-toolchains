bdd37b870d68cda24298afb554ced792bc1981c1
Sam Elliott <selliott@lowrisc.org>
2019-10-30 15:40:59 +0000

    Minor tweaks to install-crosstool-ng.sh

--- install-crosstool-ng.sh
+++ install-crosstool-ng.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
+# Copyright lowRISC contributors.
+# Licensed under the Apache License, Version 2.0, see LICENSE for details.
+# SPDX-License-Identifier: Apache-2.0
 
+# master 2019-10-02
 CROSSTOOL_NG_VERSION=3f461da11f1f8e9dcfdffef24e1982b5ffd10305
 
 mkdir -p build && cd build || exit 1
 git clone https://github.com/crosstool-ng/crosstool-ng || exit 1
-cd crosstool-ng
-git checkout $CROSSTOOL_NG_VERSION || exit 1
+cd crosstool-ng || exit 1
+git checkout --force $CROSSTOOL_NG_VERSION || exit 1
 ./bootstrap || exit 1
 ./configure --prefix=/usr/local && make && sudo make install
